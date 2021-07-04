884c19c199a54ebb65db4a09970a2a004122b170
Pedro-Alcantara-M <pedrinrn@gmail.com>
2021-07-04 09:09:10 -0300

    refactoring favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,26 +1,12 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux'
 import {selectFavorites} from '../store/store'
 import CardList from '../components/CardList';
-import { useState } from 'react';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-    
-    }
-
-  })
-)
 
 export default function Favorites () {
-  const classes = useStyles();
   const favorites = useSelector(selectFavorites)
 
- 
-
   return (
-    <div className={classes.root}>
+    <div>
       <CardList 
         characters={favorites}
       />
